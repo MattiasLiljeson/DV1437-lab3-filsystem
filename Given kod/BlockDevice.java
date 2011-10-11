cd4d22d90ea5b7f630f65b850921266427653124
Mattias Liljeson <mattiasliljeson@gmail.com>
2011-10-11 15:05:32 +0200

    Initial commit
    Given code has been added and formatted with netbeans auto-format tool.
    Layout.dia is a diagram that shows the filesystem layout.

---
+++ Given kod/BlockDevice.java
@@ -0,0 +1,10 @@
+
+public abstract class BlockDevice {
+
+    public BlockDevice() {
+    }
+
+    public abstract int writeBlock(int p_nBlockNr, byte[] p_abContents);
+
+    public abstract byte[] readBlock(int p_nBlockNr);
+}
---
+++ Given kod/TestShell.java
@@ -0,0 +1,10 @@
+
+public class TestShell {
+
+    public static void main(String[] args) {
+        MemoryBlockDevice BlockTest = new MemoryBlockDevice();
+        Filesystem FS = new Filesystem(BlockTest);
+        Shell Bash = new Shell(FS, null);
+        Bash.start();
+    }
+}
